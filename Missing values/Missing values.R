e032419fa121f0a209641054813bab956cc124d1
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-04-14 17:59:57 -0600

    practice

---
+++ Missing values/Missing values.R
@@ -0,0 +1,15 @@
+
+
+
+
+# ---------------------------------------------
+# This is a practice for Missing value
+# 14 Apr 2021
+# Pablo E. Gutiérrez-Fonseca
+# ---------------------------------------------
+#  
+Discharge.frm=read.csv("Missing values/Discharge.csv")
+Discharge.frm
+head(Discharge.frm)
+summary(Discharge.frm)
+
